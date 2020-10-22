1d51bea5291369cb7b82b5564a35e10505f522c4
Yuriy Sountsov <yuriys@kentico.com>
2020-10-21 23:36:03 -0400

    Remove extra import

--- Client/src/app/element/ChangeType.tsx
+++ Client/src/app/element/ChangeType.tsx
@@ -1,5 +1,5 @@
 import moment, { Duration } from 'moment';
-import React, { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import { useAsyncEffect } from 'use-async-effect';
 import wretch from 'wretch';
 
